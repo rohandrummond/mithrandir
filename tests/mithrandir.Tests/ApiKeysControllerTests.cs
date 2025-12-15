84058bbcbfd3ac0264fcaaa91ab2f1076511a387
Rohan Drummond <drummond.rohan@gmail.com>
2025-12-16 12:04:27 +1300

    Replace DbContext with in-memory database in testing environment

--- tests/mithrandir.Tests/ApiKeysControllerTests.cs
+++ tests/mithrandir.Tests/ApiKeysControllerTests.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using mithrandir.Data;
 using mithrandir.Models.DTOs;
 
 namespace mithrandir.Tests;
@@ -15,7 +16,31 @@ public class ApiKeysControllerTests : IClassFixture<WebApplicationFactory<Progra
 
     public ApiKeysControllerTests(WebApplicationFactory<Program> factory)
     {
-        _factory = factory;
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor =
+                    services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MithrandirDbContext>));
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<MithrandirDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using (var scope = sp.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<MithrandirDbContext>();
+                    db.Database.EnsureCreated();
+                }
+            });
+        });
+        
         _client = _factory.CreateClient();
     }
 }

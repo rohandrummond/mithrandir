affadd002937b2527991486913ea47383ee8b76e
Rohan Drummond <drummond.rohan@gmail.com>
2025-12-27 12:23:55 +1300

    Build nav bar

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { JetBrains_Mono } from 'next/font/google'
 import './globals.css'
+import Nav from '@/components/nav'
 
 const jetbrainsMono = JetBrains_Mono({
   variable: '--font-jetbrains-mono',
@@ -20,7 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${jetbrainsMono.variable} antialiased`}>
-        {children}
+        <Nav />
+        <main className="p-6">{children}</main>
       </body>
     </html>
   )
---
+++ dashboard/components/nav.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
+
+export default function Nav() {
+  return (
+    <div className="flex flex-row items-center justify-between p-6">
+      <Link href="/" className="font-bold text-xl">
+        Mithrandir
+      </Link>
+      <div className="flex flex-row items-center gap-5">
+        <a
+          href={`${process.env.DOTNET_PUBLIC_URL}/swagger/index.html`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Docs
+        </a>
+        <a
+          href="https://github.com/rohandrummond/mithrandir"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubLogoIcon className="w-[20px] h-[20px]" />
+        </a>
+      </div>
+    </div>
+  )
+}

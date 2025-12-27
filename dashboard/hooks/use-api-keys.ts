593ee073bf2c8cf57d6e16bc55786580a3b3094c
Rohan Drummond <drummond.rohan@gmail.com>
2025-12-27 19:18:30 +1300

    Update useApiKeys hook to expose mutation function from SWR

--- dashboard/hooks/use-api-keys.ts
+++ dashboard/hooks/use-api-keys.ts
@@ -3,5 +3,10 @@ import { fetcher } from '@/lib/fetcher'
 import { GetAllKeysResponse } from '@/types/responses/get-all-keys'
 
 export function useApiKeys() {
-  return useSWR('/api/keys', fetcher<GetAllKeysResponse>)
+  const { data, error, isLoading, mutate } = useSWR(
+    '/api/keys',
+    fetcher<GetAllKeysResponse>
+  )
+
+  return { data, error, isLoading, mutate }
 }

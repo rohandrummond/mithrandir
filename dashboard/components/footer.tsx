a8781c99eda35aa1575d9e16ecf20cda46256409
Rohan Drummond <drummond.rohan@gmail.com>
2026-01-14 09:00:37 +1300

    Update responsive styling in footer and layout

--- dashboard/components/footer.tsx
+++ dashboard/components/footer.tsx
@@ -1,25 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-between p-6 font-mono text-xs text-muted-foreground">
-      <div>
-        <p className="mb-1">Built with .NET, Next.js, PostgreSQL & Redis</p>
-        {/* <p className="mb-1">Built:</p>
-        <ul className="pl-4 space-y-0.5">
-          <li>Next.js</li>
-          <li>.NET</li>
-          <li>PostgreSQL</li>
-          <li>Redis</li>
-        </ul> */}
-      </div>
-
-      <div className="text-right">
-        <p className="mb-1">Running on Amazon EC2, Docker & Vercel</p>
-        {/* <p className="mb-1">Deployed:</p>
-        <ul className="pr-4 space-y-0.5">
-          <li>Vercel</li>
-          <li>AWS</li>
-        </ul> */}
-      </div>
+    <footer className="flex flex-col items-center gap-4 p-4 min-[655px]:flex-row min-[655px]:justify-between min-[655px]:gap-4 min-[655px]:p-6 font-mono text-xs text-muted-foreground">
+      <p className="text-center">
+        Built with .NET, Next.js, PostgreSQL & Redis
+      </p>
+      <p className="text-center">Running on Amazon EC2, Docker & Vercel</p>
     </footer>
   )
 }
